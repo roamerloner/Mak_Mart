bdcb696f3ebd08a0bd6a88322bb3c6904e1e45de
Asif Khan <khanarnob806@gmail.com>
2024-03-25 03:51:48 +0600

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")], 
+    prependData: `@import "./base.scss";`,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
